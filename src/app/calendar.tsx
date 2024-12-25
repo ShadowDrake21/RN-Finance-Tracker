a18ec1a371df2f6d5061bca62da95dfd08d8eb7c
Dmytro <dimka670020040@gmail.com>
2024-12-25 11:13:31 +0200

    date data fetchin and ui

--- src/app/calendar.tsx
+++ src/app/calendar.tsx
@@ -1,38 +1,36 @@
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import {
-  StyleSheet,
-  Text,
-  TextStyle,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import React, { useCallback, useLayoutEffect, useState } from 'react';
-import {
-  Calendar,
-  CalendarList,
-  Agenda,
-  DateData,
   AgendaList,
   CalendarProvider,
   ExpandableCalendar,
   WeekCalendar,
 } from 'react-native-calendars';
-import {
-  SafeAreaView,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Stack } from 'expo-router';
 import CustomSwitch from '@/components/CustomSwitch';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AgendaItem from '@/components/AgendaItem';
+import { agendaItems } from '@/dummy/agendaItems';
+import { useFetchFinances } from '@/hooks/fetch-finances.hook';
+import { useFetchFinancesByDate } from '@/hooks/fetch-finances-by-date.hook';
 
 const Page = () => {
   const { top } = useSafeAreaInsets();
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState(
+    // new Date().toISOString().split('T')[0].slice(0, 7)
+    new Date().toISOString().split('T')[0]
+  );
   const [weekView, setWeekView] = useState(false);
 
-  // const renderItem = useCallback(({ item }: any) => {
-  //   return <AgendaItem item={item} />;
-  // }, []);
+  const { items, loading, total } = useFetchFinancesByDate(selected);
+
+  useEffect(() => {
+    // console.log(items);
+  }, [items]);
+
+  const renderItem = useCallback(({ item }: any) => {
+    return <AgendaItem {...item} />;
+  }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: 'white' }}>
@@ -49,7 +47,7 @@ const Page = () => {
       <CalendarProvider
         date={new Date().toISOString().split('T')[0]}
         // onDateChanged={onDateChanged}
-        // onMonthChange={onMonthChange}
+        onDateChanged={setSelected}
         showTodayButton
         // disabledOpacity={0.6}
         // theme={todayBtnTheme.current}
@@ -73,17 +71,25 @@ const Page = () => {
         ) : (
           <ExpandableCalendar allowShadow={false} />
         )}
-        <AgendaList
-          sections={[]}
-          renderItem={() => (
-            <View>
-              <Text>something</Text>
-            </View>
-          )}
-          // scrollToNextEvent
-          sectionStyle={styles.section}
-          // dayFormat={'yyyy-MM-d'}
-        />
+        {loading ? (
+          <ActivityIndicator
+            style={{ marginTop: 20 }}
+            size="large"
+            color="black"
+          />
+        ) : (
+          <>
+            {total && <Text>{total}</Text>}
+            {items && (
+              <AgendaList
+                sections={[{ title: items[0]?.date, data: items }]}
+                renderItem={renderItem}
+                sectionStyle={styles.section}
+                scrollToNextEvent
+              />
+            )}
+          </>
+        )}
       </CalendarProvider>
     </View>
   );
