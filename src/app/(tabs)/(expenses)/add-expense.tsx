8b8616853e74ca996011e743a836415082099282
Dmytro <dimka670020040@gmail.com>
2024-12-29 09:02:48 +0200

    icons images

--- src/app/(tabs)/(expenses)/add-expense.tsx
+++ src/app/(tabs)/(expenses)/add-expense.tsx
@@ -5,23 +5,28 @@ import {
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import SumInput from '@/components/add-expense/SumInput';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import CustomSwitch from '@/components/ui/CustomSwitch';
+import TypeSwitch from '@/components/add-expense/TypeSwitch';
 
 const Page = () => {
   const { bottom } = useSafeAreaInsets();
+  const [type, setType] = useState<'expense' | 'income'>('expense');
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={{ flex: 1, paddingBottom: bottom, paddingHorizontal: 20 }}>
-        <SumInput />
-        <CustomSwitch
-          onPress={() => console.log('switch')}
-          value={true}
-          props={{ activeText: 'On', inActiveText: 'Off' }}
-        />
+      <View
+        style={{
+          flex: 1,
+          paddingBottom: bottom,
+          paddingHorizontal: 20,
+          paddingTop: 15,
+        }}
+      >
+        <SumInput style={{ paddingBottom: 30 }} />
+        <TypeSwitch type={type} setType={setType} />
       </View>
     </TouchableWithoutFeedback>
   );
